e587744a7d2acc6f4695b2383ef1b0bb3429e902
linjie <cnlinjie@foxmail.com>
2016-05-21 20:29:50 +0800

    * MD5 加密加salt

--- util/src/main/java/com/github/cnlinjie/infrastructure/util/security/MD5.java
+++ util/src/main/java/com/github/cnlinjie/infrastructure/util/security/MD5.java
@@ -1,19 +1,31 @@
 package com.github.cnlinjie.infrastructure.util.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
-public class MD5 {
-    public static String encode(String plainText) {
-        MD5 md5 = new MD5();
-        return md5.encryption(plainText);
+public final class MD5 {
+
+    private final static Logger logger = LoggerFactory.getLogger(MD5.class);
+
+    public final static String getSalt() {
+        return UUID.randomUUID().toString();
     }
 
-    public String encryption(String plainText) {
+    public  static String encode(String str) {
+        return encode(str,null);
+    }
+
+    public  static String encode(String str,String salt) {
         String re_md5 = new String();
         try {
+            str = mergePasswordAndSalt(str, salt);
             MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(plainText.getBytes());
+            md.update(str.getBytes("UTF-8"));
             byte[] b = md.digest();
 
             StringBuffer buf = new StringBuffer("");
@@ -27,13 +39,32 @@ public class MD5 {
             }
             re_md5 = buf.toString();
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            logger.error("MD5 加密错误:", e);
+        } catch (UnsupportedEncodingException e) {
+            logger.error("MD5 加密错误:", e);
         }
         return re_md5;
     }
 
+
+    /**
+     *
+     * 拼接密码与盐值
+     *
+     * @param password
+     * @param salt
+     *
+     * @return 密码{盐值}
+     */
+    private final static String mergePasswordAndSalt(String password, Object salt) {
+        if (salt == null || "".equals(salt.toString().trim())) {
+            return password;
+        }
+        return password + "{" + salt.toString() + "}";
+    }
+
     public static void main(String[] args) {
         MD5 md5 = new MD5();
-        System.out.println(md5.encryption("admin"));
+        System.out.println(md5.encode("admin",MD5.getSalt()));
     }
 }
