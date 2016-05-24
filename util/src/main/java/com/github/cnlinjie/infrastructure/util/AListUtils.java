c10983201c2f078dd0e7c4cb1bc8ade73fc7c050
linjie <cnlinjie@foxmail.com>
2016-05-25 00:33:43 +0800

    增加工具类

---
+++ util/src/main/java/com/github/cnlinjie/infrastructure/util/AListUtils.java
@@ -0,0 +1,46 @@
+package com.github.cnlinjie.infrastructure.util;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 辅助工具
+ * @author linjie
+ * @version 0.0.1
+ * @date 16-5-17
+ */
+public class AListUtils {
+
+    public AListUtils () {
+    }
+
+    public static <K>  AListUtils.AListBuilder<K> builder(ArrayList<K> arrayList) {
+        return new AListUtils.AListBuilder(arrayList);
+    }
+
+    public static AListUtils.AListBuilder<Object> builder() {
+        return new AListUtils.AListBuilder(Lists.newArrayList());
+    }
+
+    public static class  AListBuilder<K> {
+
+        private List<K> kList ;
+
+        public AListBuilder(ArrayList<K> arrayList) {
+            this.kList = arrayList;
+        }
+
+        public AListBuilder add (K k) {
+            kList.add(k);
+            return this;
+        }
+
+        public List<K> get() {
+            return kList;
+        }
+
+    }
+
+}
