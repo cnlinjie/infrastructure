fd986de69c5b8a7c8a09e6ab9e0c2ddb80c4a8a3
linjie <cnlinjie@foxmail.com>
2018-03-13 23:10:43 +0800

    UPDATE 优化相关，并列入正式版本 0.0.1

--- web/src/main/java/com/github/cnlinjie/infrastructure/web/Constant.java
+++ web/src/main/java/com/github/cnlinjie/infrastructure/web/Constant.java
@@ -10,7 +10,6 @@ public final class Constant {
     public final static int DEFAULT_ERRCODE_FAIL = -1;
     public final static int DEFAULT_ERRCODE_SUCCESS = 0;
 
-    public final static String DEFAULT_ERRCODE_FORMAT = "{\"errCode\":%s,\"msg\":\"%s\"}";
 
 
 
--- web/src/main/java/com/github/cnlinjie/infrastructure/web/model/RetModel.java
+++ web/src/main/java/com/github/cnlinjie/infrastructure/web/model/RetModel.java
@@ -1,5 +1,6 @@
 package com.github.cnlinjie.infrastructure.web.model;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.github.cnlinjie.infrastructure.web.Constant;
 import com.google.common.collect.Maps;
 
@@ -14,7 +15,8 @@ import java.util.Map;
  */
 public class RetModel implements Serializable {
 
-    public static final RetModel SUCCESS = new RetModel(Constant.DEFAULT_ERRCODE_SUCCESS,"SUCCESS", new Object());
+    @Deprecated
+    public static final RetModel SUCCESS = new RetModel(Constant.DEFAULT_ERRCODE_SUCCESS, "SUCCESS", new Object());
 
     protected Integer errCode = Constant.DEFAULT_ERRCODE_SUCCESS;
     protected Integer code = Constant.DEFAULT_ERRCODE_SUCCESS;
@@ -23,17 +25,17 @@ public class RetModel implements Serializable {
 
     protected Object data;
 
-    public Object getData () {
+    public Object getData() {
         return data;
     }
 
-    public RetModel setData (Object data) {
+    public RetModel setData(Object data) {
         this.data = data;
 
         return this;
     }
 
-    public RetModel put(String key,Object value) {
+    public RetModel put(String key, Object value) {
         if (data == null || !(data instanceof Map)) {
             data = Maps.newHashMap();
         }
@@ -41,15 +43,15 @@ public class RetModel implements Serializable {
         return this;
     }
 
-    public RetModel () {
+    public RetModel() {
     }
 
 
-    public RetModel (Object data) {
+    public RetModel(Object data) {
         this.data = data;
     }
 
-    public RetModel (Integer errCode, String msg, Object data) {
+    public RetModel(Integer errCode, String msg, Object data) {
         this.errCode = errCode;
         this.code = errCode;
         this.msg = msg;
@@ -57,51 +59,55 @@ public class RetModel implements Serializable {
     }
 
 
-    public RetModel (Integer errCode, String msg) {
+    public RetModel(Integer errCode, String msg) {
         this.errCode = errCode;
         this.code = errCode;
         this.msg = msg;
     }
 
-    public Integer getCode () {
+    public Integer getCode() {
         return code;
     }
 
-    public void setCode (Integer code) {
+    public void setCode(Integer code) {
         this.errCode = code;
         this.code = code;
     }
 
-    public Integer getErrCode () {
+
+    @JSONField(serialize = false)
+    @Deprecated
+    public Integer getErrCode() {
         return errCode;
     }
 
-    public RetModel setErrCode (Integer errCode) {
+    @Deprecated
+    public RetModel setErrCode(Integer errCode) {
         this.code = errCode;
         this.errCode = errCode;
         return this;
     }
 
-    public String getMsg () {
+    public String getMsg() {
         return msg;
     }
 
-    public RetModel setMsg (String msg) {
+    public RetModel setMsg(String msg) {
         this.msg = msg;
         return this;
     }
 
-    public static RetModel Put(String key,Object value){
+    public static RetModel Put(String key, Object value) {
         return get().put(key, value);
     }
 
 
-    public static RetModel get(){
-        return new RetModel(Constant.DEFAULT_ERRCODE_SUCCESS,"SUCCESS", new HashMap<String,Object>());
+    public static RetModel get() {
+        return new RetModel(Constant.DEFAULT_ERRCODE_SUCCESS, "SUCCESS", new HashMap<String, Object>());
     }
 
-    public static RetModel error(){
-        return new RetModel(Constant.DEFAULT_ERRCODE_FAIL,"FAIL", new HashMap<String,Object>());
+    public static RetModel error() {
+        return new RetModel(Constant.DEFAULT_ERRCODE_FAIL, "FAIL", new HashMap<String, Object>());
     }
 
 }
